0b6f0fb33747afaa756544e2738abee45c2fc2e6
Bibi <beeskywalker@yahoo.com>
2020-05-04 15:50:34 +0300

    Start Page created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,31 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Button login, signup;
+
     public static void main(String[] args) {
+
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
 
+        primaryStage.setTitle("Portal consultatii");
+        login=new Button("Log In");
+        signup=new Button("Sign Up");
+        StackPane layout=new StackPane();
+        layout.getChildren().add(login);
+        layout.getChildren().add(signup);
+        Scene scene=new Scene(layout,300,250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
