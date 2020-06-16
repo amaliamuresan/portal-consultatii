739203ee6cd0c7f5112a1d071cd6e85895cb0284
Amalia Muresan <amaliamuresan99@yahoo.com>
2020-06-16 12:40:41 +0300

    PCD-8 filtered list of doctors done

--- src/main/java/MainPacient.java
+++ src/main/java/MainPacient.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -5,18 +8,35 @@ import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class MainPacient extends Application {
 
+    private static Object String;
     private Stage window;
+    static ListView<String> listView=new ListView<>();
+    static GridPane gridLayout = new GridPane();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         launch(args);
+
     }
 
     @Override
@@ -26,10 +46,14 @@ public class MainPacient extends Application {
 
         window.setTitle("Pagina Principala");
 
-        Button mainpg,cereri,solicita;
+        Button mainpg,cereri,solicita, filtreazaBtn;
         mainpg=new Button("Pagina Principala");
         cereri=new Button("Cererile mele");
         solicita=new Button("Cere Serviciu");
+        filtreazaBtn = new Button("Filtreaza");
+        Button okBtn = new Button("OK");
+        TextField specialtyTF = new TextField();
+        specialtyTF.setPromptText("Alegeti specializarea");
 
         mainpg.setPrefWidth(140);
         cereri.setPrefWidth(130);
@@ -37,30 +61,60 @@ public class MainPacient extends Application {
 
         Label medici=new Label("Medici disponibili:");
 
-        GridPane gridLayout = new GridPane();
+        //GridPane gridLayout = new GridPane();
         gridLayout.setPadding(new Insets(15, 15, 15, 15));
         gridLayout.setVgap(7);
         gridLayout.setHgap(1);
 
-        ListView<String> listView=new ListView<>();
+
+
         MainPacient.addNumeMedici(listView);
 
-        gridLayout.getChildren().addAll(mainpg,cereri, medici,listView,solicita);
+        gridLayout.getChildren().addAll(mainpg,cereri, medici,listView,solicita, filtreazaBtn);
 
         gridLayout.setConstraints(mainpg, 0, 0);
         gridLayout.setConstraints(cereri, 0, 1);
         gridLayout.setConstraints(medici, 1, 0);
         gridLayout.setConstraints(listView,1,1);
         gridLayout.setConstraints(solicita,1,2);
+        gridLayout.setConstraints(filtreazaBtn, 0, 2);
 
         gridLayout.setValignment(cereri,VPos.TOP);
 
         gridLayout.setHgap(10);
 
+
+
         Scene sceneMain = new Scene(gridLayout, 500, 350);
         window.setScene(sceneMain);
 
         window.show();
+        //MainPacient.filterMedici("s2");
+
+        filtreazaBtn.setOnAction( e -> {
+            HBox hb = new HBox(10);
+            hb.getChildren().addAll(specialtyTF, okBtn);
+            hb.setAlignment(Pos.BASELINE_CENTER);
+            hb.setPadding(new Insets(5, 5,5,5));
+            Stage wi= new Stage();
+            wi.setTitle("Filtreaza");
+
+            Scene sc = new Scene(hb, 300, 50);
+            wi.setScene(sc);
+            wi.show();
+
+            okBtn.setOnAction( r -> {
+                try {
+                    MainPacient.filterMedici(specialtyTF.getText());
+
+
+                    wi.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            });
+
+        });
     }
 
     private static void addNumeMedici(ListView<String> list){
@@ -70,4 +124,41 @@ public class MainPacient extends Application {
                 list.getItems().add(user.getUsername());
             }
     }
+
+    private static void filterMedici(String spec) throws IOException {
+        //ListView<String> filtredList = new ListView<>();
+        listView.getItems().clear();
+        for (JsonUser user: SignUp.obj)
+            if (user.getRole().equals("Medic")) {
+                String filename = "Users/" + user.getUsername() + ".json";
+                ObjectMapper objectMapper = new ObjectMapper();
+                File file = new File(filename);
+                JSONObject jsonObject = parseJSONFile(filename);
+                String speciality = (java.lang.String) jsonObject.get("specialitate");
+                if(speciality.equals(spec))
+                    listView.getItems().add(user.getUsername());
+
+
+            }
+
+        //listView = filtredList;
+           // listView.refresh();
+        //listView.getItems().clear();
+
+        //gridLayout.setConstraints(filtredList,1,1);
+
+
+
+
+    }
+
+    public static JSONObject parseJSONFile(String filename) throws JSONException, IOException {
+        String content = new String(Files.readAllBytes(Paths.get(filename)));
+        return new JSONObject(content);
+    }
+
+
+
+
+
 }
