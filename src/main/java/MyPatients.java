28635bd8ef83f9eb6bef4b53c36887aa6d46cb71
Bibi <beeskywalker@yahoo.com>
2020-06-20 19:25:12 +0300

    PCD-15 Externarea pacientilor e gata

--- src/main/java/MyPatients.java
+++ src/main/java/MyPatients.java
@@ -1,14 +1,17 @@
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class MyPatients {
@@ -19,6 +22,7 @@ public class MyPatients {
         Button extern=new Button("Externeaza");
 
         listView.setMaxWidth(250);
+        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
         HBox hBox=new HBox();
         hBox.getChildren().addAll(mainpg,extern);
@@ -44,6 +48,15 @@ public class MyPatients {
                     ioException.printStackTrace();
                 }
         });
+        extern.setOnAction(e -> {
+            if(listView.getSelectionModel().isEmpty()) {
+                Label neselectat = new Label("Selectati un pacient, apoi faceti externati-l!!!");
+                vBox.getChildren().add(neselectat);
+            }
+            else {
+                MyPatients.externeazaPacient(listView);
+            }
+        });
     }
 
     private static void updatePatientsList(ListView<String> listView){
@@ -53,6 +66,27 @@ public class MyPatients {
             JSONArray pacienti=jsonObject.getJSONArray("Pacienti");
             for (int i=0;i<pacienti.length();i++)
                 listView.getItems().add(pacienti.getString(i));
+        }catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+
+    private static void externeazaPacient(ListView<String> listView){
+        String filename = "Users/" + LogIn.loggedUser.getUsername() + ".json";
+        try {
+            JSONObject jsonObject= MainPacient.parseJSONFile(filename);
+            JSONArray pacienti=jsonObject.getJSONArray("Pacienti");
+            ObservableList<String> pac=listView.getSelectionModel().getSelectedItems();
+            for (String pacient : pac)
+                for (int i=0;i<pacienti.length();i++)
+                    if(pacient.equals(pacienti.getString(i)))
+                        pacienti.remove(i);
+            jsonObject.remove("Pacienti");
+            jsonObject.put("Pacienti",pacienti);
+            FileWriter fileWriter=new FileWriter(filename);
+            fileWriter.write(jsonObject.toString());
+            fileWriter.flush();
+            //MyPatients.updatePatientsList(listView);
         }catch (IOException ioException){
             ioException.printStackTrace();
         }
