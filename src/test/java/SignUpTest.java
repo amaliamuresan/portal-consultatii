c9d5292a354413addd4970f5c573fcaf9434252f
Bibi <beeskywalker@yahoo.com>
2020-06-20 23:15:08 +0300

    Some changes

---
+++ src/test/java/SignUpTest.java
@@ -0,0 +1,11 @@
+/*import junit.framework.TestCase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SignUpTest extends TestCase {
+    public void testInitSignUp(){
+        assertEquals(false,SignUp.initSignUp(""));
+        assertEquals(true,SignUp.initSignUp("minea"));
+    }
+
+}*/
